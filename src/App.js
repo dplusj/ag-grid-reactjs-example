3f47e5049d6186c16e4ff9f66383e282ede4ab64
dplusj <jerrydeng.plus@gmail.com>
2020-03-08 20:00:53 +0800

    turn on enterprise mode

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
 
-import 'ag-grid-enterprise';
+import {ModuleRegistry, AllModules} from '@ag-grid-enterprise/all-modules';
+
 
 import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import "@ag-grid-enterprise/all-modules/dist/styles/ag-grid.css";
+import "@ag-grid-enterprise/all-modules/dist/styles/ag-theme-balham.css";
 
+ModuleRegistry.registerModules(AllModules);
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       columnDefs: [{
-        headerName: "Make", field: "make", sortable: true, filter: true, rowGroup: true, hide:true
+        headerName: "Make", field: "make", pivot:true, 
       }, {
-        headerName: "Model", field: "model", sortable: true, filter: true
+        headerName: "Model", field: "model", pivot:true,
       }, {
-        headerName: "Price", field: "price", sortable: true, filter: true
+        headerName: "Price", field: "price", aggFunc: 'sum'
       }],
       rowData: null,
       autoGroupColumnDef: {
@@ -56,11 +58,16 @@ class App extends Component {
       <button onClick={this.onButtonClick}>Get Selected Rows</button>
         <AgGridReact
           columnDefs={this.state.columnDefs}
-          autoGroupColumnDef={this.state.autoGroupColumnDef}
           rowData={this.state.rowData}
           rowSelection='multiple'
           onGridReady={params => this.gridApi = params.api}
-          groupSelectsChildren={true} >
+          groupSelectsChildren={true}
+          modules={AllModules}
+          enableSorting={true}
+          enableFilter={true}
+          pivotMode={true}
+          pivotPanelShow='always' 
+           >
         </AgGridReact>
       </div>
     );
